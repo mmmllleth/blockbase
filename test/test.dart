4f1ab6e172cb00b7913a67c8edf0c077ae3cd133
mmmllltest1 <mmmllltest57@gmail.com>
2023-08-26 19:29:51 +0800

    feat: update ios sdk

--- test/test.dart
+++ test/test.dart
@@ -1,6 +1,5 @@
+import "package:blockbase/blockbase.dart";
 import "package:ffi/ffi.dart";
-
-import "../lib/blockbase.dart";
 
 import "../lib/ton/ton.dart";
 
@@ -17,4 +16,16 @@ void main() {
   final pk = Ton().recoverAccount(
       "ad3336058ffa068baeb6f0088357ef3c7d78b00ce5e83bb0544c37fa68a400b4925bda8623d366a2da2f53e3a55165dc5ea2e65e22a7615b792ab7822c0d241b");
   print("pk $pk");
+
+  bindings.ChangeTonWallet(
+      "van peasant sand today height scrap arrange park brass spoon invest wave spin brave sudden width clerk mean slim whip you hotel embrace aim"
+          .toNativeUtf8()
+          .cast());
+
+  bindings.TON_transfer21(
+    "EQDFEYVOyyRzugwEL7qGfYHE4jWMWRsySKtzlxa2TeXu8P4S".toNativeUtf8().cast(),
+    "1".toNativeUtf8().cast(),
+    "kQDaGs13zjwtDBX22xE5KOI6U_iphXU340eEpDX9N818GL2N".toNativeUtf8().cast(),
+    "token".toNativeUtf8().cast(),
+  );
 }
